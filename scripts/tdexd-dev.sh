56cb8cd3298a3f3b14e4cdb45caa8871427ddbc7
altafan <pietro@vulpem.com>
2021-04-02 11:49:05 +0200

    Fix script and logs

--- scripts/tdexd-dev.sh
+++ scripts/tdexd-dev.sh
@@ -2,31 +2,44 @@
 
 set -e
 
-tdex='docker exec -it tdexd tdex '
+echo "creating wallet"
+
+tdex='./build/tdex-darwin-amd64'
 $tdex config init --network "regtest"
-$tdex init --seed "wrestle actress dirt spice ridge bone depart leisure fruit shoot elite mistake play supply inhale million tennis harvest merit anchor shaft desert organ salute" --password secret
-$tdex unlock --password secret
+$tdex init --seed "wrestle actress dirt spice ridge bone depart leisure fruit shoot elite mistake play supply inhale million tennis harvest merit anchor shaft desert organ salute" --password secret &>/dev/null
+
+echo ""
+echo "unlocking wallet"
+
+$tdex unlock --password secret &>/dev/null
 
 addressfee=$($tdex depositfee | jq -r '.address_with_blinding_key[0].address')
-echo $addressfee
+
+echo ""
+echo "fee address: ${addressfee}"
 
 feetxid=$(nigiri faucet --liquid $addressfee | grep '^txId: ' | cut -d ':' -f 2 | tr -d ' ')
 sleep 5
 
 feevout=$(nigiri rpc --liquid gettransaction $feetxid | jq -r '.details[0].vout')
+echo "fee outpoint: ${feetxid} ${feevout}"
 
-$tdex claimfee --outpoints '[{"hash":"'$feetxid'", "vout":'$feevout'}]'
+$tdex claimfee --outpoints '[{"hash":"'$feetxid'", "index":'$feevout'}]'
 sleep 1
 
 addressmarket=$($tdex depositmarket | jq -r '.addresses[0]')
-echo $addressmarket
+
+echo ""
+echo "market address: ${addressmarket}"
 
 btctxid=$(nigiri faucet --liquid $addressmarket | grep '^txId: ' | cut -d ':' -f 2 | tr -d ' ')
 sleep 5
 
 btcvout=$(nigiri rpc --liquid gettransaction $btctxid | jq -r '.details[0].vout')
 
-mintresponse=$(curl -X POST -H 'Content-Type: application/json' -d '{"address":"'$addressmarket'", "amount": 100}' "http://localhost:3001/mint")
+echo "market base outpoint: ${btctxid} ${btcvout}"
+
+mintresponse=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"address":"'$addressmarket'", "quantity": 100}' "http://localhost:3001/mint")
 sleep 5
 
 shitcoin=$(echo $mintresponse | jq -r '.asset')
@@ -34,10 +47,17 @@ shittxid=$(echo $mintresponse | jq -r '.txId')
 
 shitvout=$(nigiri rpc --liquid gettransaction $shittxid | jq -r '.details[0].vout')
 
-$tdex config set base_asset 5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225
-$tdex config set quote_asset $shitcoin
+shitcoin=$(echo $mintresponse | jq -r '.asset')
+echo "market quote asset: ${shitcoin}"
+echo "market quote outpoint: ${shittxid} ${shitvout}"
 
-$tdex claimmarket --outpoints '[{"hash":"'$btctxid'", "vout":'$btcvout'}, {"hash":"'$shittxid'", "vout":'$shitvout'}]'
+$tdex config set base_asset 5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225 &>/dev/null
+$tdex config set quote_asset $shitcoin &>/dev/null
+
+$tdex claimmarket --outpoints '[{"hash":"'$btctxid'", "index":'$btcvout'}, {"hash":"'$shittxid'", "index":'$shitvout'}]'
 sleep 1
 
+
+echo ""
+echo "opening market"
 $tdex open
