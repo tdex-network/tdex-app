1f63e702c8106e341acf0b6ca3ba802643b1f6ef
stephane <rochestephane@yahoo.fr>
2021-04-27 18:33:00 +0100

    Display Deposit button and + button. Display L-BTC asset by default if empty balance.

--- src/pages/Wallet/index.tsx
+++ src/pages/Wallet/index.tsx
@@ -9,9 +9,14 @@ import {
   IonListHeader,
   IonButton,
   useIonViewWillEnter,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from '@ionic/react';
 import React, { useEffect, useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
+import { addCircleOutline } from 'ionicons/icons';
 import { network } from '../../redux/config';
 import { CurrencyIcon } from '../../components/icons';
 import { BalanceInterface } from '../../redux/actionTypes/walletActionTypes';
@@ -108,12 +113,27 @@ const Wallet: React.FC<WalletProps> = ({
   useEffect(() => {
     const main = [];
     const secondary = [];
-    for (const balance of balances) {
-      if (getMainAsset(balance.asset)) {
-        main.push(balance);
-        continue;
+    if (balances.length) {
+      for (const balance of balances) {
+        if (getMainAsset(balance.asset)) {
+          main.push(balance);
+          continue;
+        }
+        secondary.push(balance);
       }
-      secondary.push(balance);
+    } else {
+      // Display L-BTC with empty balance
+      const [lbtc] = MAIN_ASSETS.filter(
+        (a) => a.ticker === 'L-BTC' && a.chain === network.chain
+      ).map((a) => {
+        return {
+          asset: a.assetHash,
+          ticker: a.ticker,
+          amount: 0,
+          precision: 8,
+        };
+      });
+      main.push(lbtc);
     }
     setMainAssets(main);
     setSecondaryAssets(secondary);
@@ -128,135 +148,162 @@ const Wallet: React.FC<WalletProps> = ({
     <IonPage>
       <div className="gradient-background" />
       <IonContent className="wallet-content">
-        <Refresher />
-        <div className="diagram">
-          <CircleDiagram />
-        </div>
-        <IonHeader className="header wallet">
-          <IonToolbar>
-            <IonTitle>Wallet</IonTitle>
-          </IonToolbar>
-          <div className="total-info">
-            <div className="header-info wallet">
-              <p className="info-heading">Total balance</p>
-              <p className="info-amount" aria-label="main-balance">
-                {totalLBTC
-                  ? fromSatoshiFixed(totalLBTC.amount, 8, undefined, lbtcUnit)
-                  : '0.00'}
-                <span>{lbtcUnit}</span>
-              </p>
-              {totalLBTC && prices[LBTC_COINGECKOID] && (
-                <p className="info-amount-converted">
-                  {`${(
-                    fromSatoshi(totalLBTC.amount) * prices[LBTC_COINGECKOID]
-                  ).toFixed(2)} ${currency.toUpperCase()}`}
-                </p>
-              )}
-            </div>
+        <IonGrid>
+          <Refresher />
+          <div className="diagram">
+            <CircleDiagram />
           </div>
-        </IonHeader>
-        <IonList scroll-y={true}>
-          <IonListHeader>
-            Asset list
-            <IonButton
-              className="coin-action-button ml-auto small-button"
-              onClick={() => {
-                if (backupDone) {
-                  setExchangeSearchOpen(true);
-                  return;
-                }
-                setBackupModal(true);
-              }}
-            >
-              Deposit
-            </IonButton>
-          </IonListHeader>
+          <IonHeader className="header wallet">
+            <IonToolbar>
+              <IonTitle>Wallet</IonTitle>
+            </IonToolbar>
+            <div className="total-info">
+              <div className="header-info wallet">
+                <p className="info-heading">Total balance</p>
+                <p className="info-amount" aria-label="main-balance">
+                  {totalLBTC.amount
+                    ? fromSatoshiFixed(totalLBTC.amount, 8, undefined, lbtcUnit)
+                    : '0.00'}
+                  <span>{lbtcUnit}</span>
+                </p>
+                {totalLBTC && prices[LBTC_COINGECKOID] && (
+                  <p className="info-amount-converted">
+                    {`${(
+                      fromSatoshi(totalLBTC.amount) * prices[LBTC_COINGECKOID]
+                    ).toFixed(2)} ${currency.toUpperCase()}`}
+                  </p>
+                )}
+              </div>
+            </div>
+          </IonHeader>
+          <IonList scroll-y={true} className="ion-padding-bottom">
+            <IonListHeader className="ion-no-margin">
+              <IonRow className="ion-align-items-center">
+                <IonCol size="6">Asset list</IonCol>
+                <IonCol size="6" className="ion-text-right">
+                  <IonButton
+                    className="ion-no-padding"
+                    onClick={() => {
+                      if (backupDone) {
+                        setExchangeSearchOpen(true);
+                        return;
+                      }
+                      setBackupModal(true);
+                    }}
+                  >
+                    <IonIcon
+                      icon={addCircleOutline}
+                      slot="icon-only"
+                      color="success"
+                    />
+                  </IonButton>
+                </IonCol>
+              </IonRow>
+            </IonListHeader>
 
-          {mainAssets
-            .concat(secondaryAssets)
-            .map((balance: BalanceInterface) => {
-              const fiatValue = getFiatValue(balance);
-              return (
-                <IonItem
-                  aria-label={balance.ticker}
-                  key={balance.asset}
-                  onClick={() => {
-                    history.push(`/operations/${balance.asset}`);
-                  }}
-                >
-                  <div className="item-main-info">
-                    <div className="item-start">
-                      <CurrencyIcon currency={balance.ticker} />
-                      <div className="item-name">
-                        <div
-                          className="main-row"
-                          aria-label={`${balance.ticker}-asset`}
-                        >
-                          {balance.coinGeckoID
-                            ? capitalizeFirstLetter(balance.coinGeckoID)
-                            : balance.ticker}
+            {mainAssets
+              .concat(secondaryAssets)
+              .map((balance: BalanceInterface) => {
+                const fiatValue = getFiatValue(balance);
+                return (
+                  <IonItem
+                    aria-label={balance.ticker}
+                    key={balance.asset}
+                    onClick={() => {
+                      history.push(`/operations/${balance.asset}`);
+                    }}
+                  >
+                    <div className="item-main-info">
+                      <div className="item-start">
+                        <CurrencyIcon currency={balance.ticker} />
+                        <div className="item-name">
+                          <div
+                            className="main-row"
+                            aria-label={`${balance.ticker}-asset`}
+                          >
+                            {balance.coinGeckoID
+                              ? capitalizeFirstLetter(balance.coinGeckoID)
+                              : balance.ticker}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="item-end">
-                      <div className="first-col">
-                        <div
-                          className="main-row"
-                          aria-label={`${balance.ticker}-amount`}
-                        >
-                          {fromSatoshiFixed(
-                            balance.amount,
-                            balance.precision,
-                            balance.precision,
-                            balance.ticker === 'L-BTC' ? lbtcUnit : undefined
+                      <div className="item-end">
+                        <div className="first-col">
+                          <div
+                            className="main-row"
+                            aria-label={`${balance.ticker}-amount`}
+                          >
+                            {fromSatoshiFixed(
+                              balance.amount,
+                              balance.precision,
+                              balance.precision,
+                              balance.ticker === 'L-BTC' ? lbtcUnit : undefined
+                            )}
+                          </div>
+                          <div className="sub-row">
+                            {fiatValue < 0
+                              ? fiatValue === UNKNOWN
+                                ? ''
+                                : 'loading'
+                              : fiatValue?.toFixed(2)}
+                          </div>
+                        </div>
+                        <div className="second-col">
+                          <div className="main-row accent">
+                            {balance.ticker === 'L-BTC'
+                              ? lbtcUnit
+                              : balance.ticker}
+                          </div>
+                          {fiatValue >= 0 && (
+                            <div className="sub-row">
+                              {currency.toUpperCase()}
+                            </div>
                           )}
                         </div>
-                        <div className="sub-row">
-                          {fiatValue < 0
-                            ? fiatValue === UNKNOWN
-                              ? ''
-                              : 'loading'
-                            : fiatValue?.toFixed(2)}
-                        </div>
-                      </div>
-                      <div className="second-col">
-                        <div className="main-row accent">
-                          {balance.ticker === 'L-BTC'
-                            ? lbtcUnit
-                            : balance.ticker}
-                        </div>
-                        {fiatValue >= 0 && (
-                          <div className="sub-row">
-                            {currency.toUpperCase()}
-                          </div>
-                        )}
                       </div>
                     </div>
-                  </div>
-                </IonItem>
-              );
-            })}
-        </IonList>
+                  </IonItem>
+                );
+              })}
+          </IonList>
 
-        <ExchangeSearch
-          assets={assetsWithTicker}
-          currency={currency}
-          isOpen={ExchangeSearchOpen}
-          close={() => setExchangeSearchOpen(false)}
-          prices={prices}
-          setAsset={handleAssetSelection}
-          isDepositSearch={true}
-        />
+          {!totalLBTC.amount && (
+            <div className="ion-text-center ion-padding-vertical">
+              <IonButton
+                className="main-button"
+                onClick={() => {
+                  if (backupDone) {
+                    setExchangeSearchOpen(true);
+                    return;
+                  }
+                  setBackupModal(true);
+                }}
+              >
+                DEPOSIT ASSETS
+              </IonButton>
+            </div>
+          )}
 
-        {!backupDone && (
-          <BackupModal
-            title="Backup your seed before deposit"
-            description="Take time to keep your secret words in a safe place before deposit funds."
-            removeSkipBtn={true}
-            isOpen={backupModal}
-            onClose={() => setBackupModal(false)}
+          <ExchangeSearch
+            assets={assetsWithTicker}
+            currency={currency}
+            isOpen={ExchangeSearchOpen}
+            close={() => setExchangeSearchOpen(false)}
+            prices={prices}
+            setAsset={handleAssetSelection}
+            isDepositSearch={true}
           />
-        )}
+
+          {!backupDone && (
+            <BackupModal
+              title="Backup your seed before deposit"
+              description="Take time to keep your secret words in a safe place before deposit funds."
+              removeSkipBtn={true}
+              isOpen={backupModal}
+              onClose={() => setBackupModal(false)}
+            />
+          )}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
