c31fa1fec538d81bdd041a0367c4e15546e2cad5
Stéphane Roche <rochestephane@yahoo.fr>
2021-06-02 07:21:19 +0100

    Set flag seed-backup in storage after wallet restoration (#272)
    * Set flag _cap_tdex-app-seed-backup in storage
    
    * lint

---
+++ src/redux/containers/restoreWalletContainer.tsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import RestoreWallet from '../../pages/RestoreWallet';
+import { setSeedBackupFlag } from '../../utils/storage-helper';
+import { setIsBackupDone } from '../actions/appActions';
+
+const mapStateToProps = (state: any) => {
+  return {
+    backupDone: state.app.backupDone,
+  };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    setIsBackupDone: (done: boolean) => {
+      setSeedBackupFlag(done);
+      dispatch(setIsBackupDone(done));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestoreWallet);
