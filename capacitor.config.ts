db5d2451d5b949cb5977dee0f7763d92ada4a71f
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-07-07 11:13:24 +0200

    Rename app name for Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.sevenlabs.app',
-  appName: 'tdex-app',
+  appName: 'TDEX',
   bundledWebRuntime: false,
   webDir: 'build',
   plugins: {
