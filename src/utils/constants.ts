3e8a8d8bc246d1cad7ea52d20015e1b124ad1a7c
stephane <rochestephane@yahoo.fr>
2021-08-10 16:51:20 +0100

    Make sure to set the correct LBTC_ASSET hash!

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -36,9 +36,9 @@ export const LBTC_ASSET: AssetConfig =
         ticker: LBTC_TICKER,
         assetHash:
           // FedPegScript => OP_TRUE
-          '5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225',
-        // FedPegScript => 1 pubKey
-        //'056293ee681516f2d61bb7ce63030351d5e02d61aef9fb00d30f27f55d935b18',
+          //'5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225',
+          // FedPegScript => 1 pubKey
+          '056293ee681516f2d61bb7ce63030351d5e02d61aef9fb00d30f27f55d935b18',
         color: LBTC_COLOR,
         precision: 8,
         chain: 'regtest',
