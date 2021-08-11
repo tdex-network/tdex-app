4ee179797c0c7a1aad6972b9cf4c4f12e2d24486
Stéphane Roche <rochestephane@yahoo.fr>
2021-08-11 12:16:54 +0100

    Increase Cypress sleep times (#389)
    * Increase sleep times

--- cypress/integration/main.spec.ts
+++ cypress/integration/main.spec.ts
@@ -30,20 +30,18 @@ describe('trade', () => {
     cy.launchWallet();
     cy.get('[data-cy=tab-exchange]').click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(10_000);
+    cy.wait(5_000);
     cy.get('[data-cy=exchange-send-input]').children().type('1');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(5000);
+    cy.wait(2000);
     cy.get('[data-cy=exchange-confirm-btn]').contains('CONFIRM').click();
     cy.get('[data-cy=description-p]').should(
       'contain',
       'Enter your secret PIN to send 1 L-BTC and receive',
     );
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(3000);
+    cy.wait(2000);
     cy.get('[data-cy=pin-input]').children().type(pin);
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(3000);
     cy.get('[data-cy=header-title]').should('contain.text', 'TRADE SUMMARY');
     cy.get('[data-cy=trade-summary-sent-amount]').should('contain.text', '-1');
     cy.get('[data-cy=trade-summary-btn]')
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -20,11 +20,11 @@ Cypress.Commands.add('launchWallet', () => {
     .then(() => {
       faucet(firstAddress, 100).catch(console.error);
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1000);
+      cy.wait(10_000);
     })
     .then(() => {
       cy.get('[data-cy=pin-input]').children().type(pin);
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(5000);
+      cy.wait(5_000);
     });
 });
