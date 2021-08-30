eb6c59b476763d0d79c8929fb6d9720165a35e7c
stephane <rochestephane@yahoo.fr>
2021-08-30 17:13:44 +0100

    Catch error price fetching

--- src/redux/sagas/ratesSaga.ts
+++ src/redux/sagas/ratesSaga.ts
@@ -6,22 +6,26 @@ import { getPriceFromCoinGecko } from '../services/ratesService';
 import type { CoinGeckoPriceResult } from '../services/ratesService';
 
 function* fetchRates() {
-  const currency: CurrencyInterface['value'] = yield select(
-    ({ settings }: { settings: SettingsState }) => settings.currency.value
-  );
-  const currencies = [currency];
-  if (currency !== 'btc') {
-    currencies.push('btc');
+  try {
+    const currency: CurrencyInterface['value'] = yield select(
+      ({ settings }: { settings: SettingsState }) => settings.currency.value
+    );
+    const currencies = [currency];
+    if (currency !== 'btc') {
+      currencies.push('btc');
+    }
+    const coinGeckoResult: CoinGeckoPriceResult = yield call(getPriceFromCoinGecko, currencies);
+    const prices: Record<string, number> = {};
+    const lbtcPrices: Record<string, number> = {};
+    for (const crypto of Object.keys(coinGeckoResult)) {
+      prices[crypto] = coinGeckoResult[crypto][currency];
+      lbtcPrices[crypto] = coinGeckoResult[crypto]['btc'];
+    }
+    yield put(setPrices(prices));
+    yield put(setLBTCPrices(lbtcPrices));
+  } catch (err) {
+    console.error(err);
   }
-  const coinGeckoResult: CoinGeckoPriceResult = yield call(getPriceFromCoinGecko, currencies);
-  const prices: Record<string, number> = {};
-  const lbtcPrices: Record<string, number> = {};
-  for (const crypto of Object.keys(coinGeckoResult)) {
-    prices[crypto] = coinGeckoResult[crypto][currency];
-    lbtcPrices[crypto] = coinGeckoResult[crypto]['btc'];
-  }
-  yield put(setPrices(prices));
-  yield put(setLBTCPrices(lbtcPrices));
 }
 
 export function* ratesWatcherSaga(): Generator<any, any, any> {
