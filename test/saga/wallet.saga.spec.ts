cad79373e2ff75de6b3d59c6f989dceaebd591c0
Janaka-Steph <rochestephane@yahoo.fr>
2022-01-31 14:20:33 +0000

    Fix tests

--- test/saga/wallet.saga.spec.ts
+++ test/saga/wallet.saga.spec.ts
@@ -4,6 +4,7 @@ import type { AddressInterface } from 'ldk';
 import { fetchAndUnblindUtxos } from 'ldk';
 import type { PutEffect, StrictEffect } from 'redux-saga/effects';
 import type { UnblindedOutput } from 'tdex-sdk';
+import { getSats } from 'tdex-sdk';
 
 import { SET_UTXO, DELETE_UTXO } from '../../src/redux/actions/walletActions';
 import { outpointToString } from '../../src/redux/reducers/walletReducer';
@@ -31,7 +32,7 @@ describe('wallet saga', () => {
       const callEffect = gen.next().value as StrictEffect<IteratorResult<UnblindedOutput, number>>;
       const result = await callEffect.payload.fn();
       const put = gen.next(result).value as PutEffect<ActionType>;
-      expect(put.payload.action.payload.value).toEqual(100000000);
+      expect(getSats(put.payload.action.payload)).toEqual(100000000);
       expect(put.payload.action.type).toEqual(SET_UTXO);
     });
 
