d35a8c98fecccde3d0c74a579a735e6db14881ac
tiero <3596602+tiero@users.noreply.github.com>
2022-02-08 15:17:19 +0100

    tag.sh: git push version tag to origin

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -9,7 +9,11 @@ version=$(cat package.json| jq --raw-output .version)
 echo "Bump iOS and Android"
 yarn bump
 
-echo "Push new tag..."
 git add .
 git commit -m "Bump $version"
 git push origin master
+
+echo "Push new tag..."
+#git tag -a v$version -m "Release v$version"
+git push origin v$version
+
