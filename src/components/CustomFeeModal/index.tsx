3b5b94eeb0ecec9d5c26d391504c7507670be9f6
libreleon <libreleon@users.noreply.github.com>
2022-02-15 00:10:26 -0800

    Clean up CSS & use utility classes

--- src/components/CustomFeeModal/index.tsx
+++ src/components/CustomFeeModal/index.tsx
@@ -26,9 +26,9 @@ const CustomFeeModal: React.FC<CustomFeeModalProps> = ({ isOpen, close, recommen
   const [showCustomFeeInput, setShowCustomFeeInput] = useState(false);
   const [selectedItem, setSelectedItem] = useState<'slow' | 'medium' | 'fast' | 'custom'>('slow');
 
-  const slowFee = recommendedFees?.["25"] || 0.1;
-  const mediumFee = recommendedFees?.["10"] || 0.1;
-  const fastFee = recommendedFees?.["1"] || 0.1;
+  const slowFee = recommendedFees?.['25'] || 0.1;
+  const mediumFee = recommendedFees?.['10'] || 0.1;
+  const fastFee = recommendedFees?.['1'] || 0.1;
 
   const slowFeeSelected = selectedItem === 'slow' ? 'active' : '';
   const mediumFeeSelected = selectedItem === 'medium' ? 'active' : '';
@@ -36,69 +36,67 @@ const CustomFeeModal: React.FC<CustomFeeModalProps> = ({ isOpen, close, recommen
 
   return (
     <IonModal cssClass="custom-fee-modal modal-medium" isOpen={isOpen} onDidDismiss={close}>
-      <IonHeader className="ion-no-border">
-        <div>
-          <label className="fee-modal-navbar ion-justify-content-end">
-            <IonIcon icon={closeSharp} color="light-contrast" onClick={close} />
-          </label>
-        </div>
+      <IonHeader className="ion-no-border py-2 px-4 d-flex ion-justify-content-end">
+        <label>
+          <IonIcon icon={closeSharp} color="light-contrast" onClick={close} />
+        </label>
       </IonHeader>
-      <IonContent className="fee-modal-content">
+      <IonContent className="mt-3">
         <IonList>
           <IonItem
-            className={`ion-no-margin ${fastFeeSelected}`}
+            className={`ion-no-margin mt-2 mb-3 mx-7 d-flex ion-justify-content-between ${fastFeeSelected}`}
             onClick={() => {
               setSelectedFee(fastFee);
               setSelectedItem('fast');
               close();
             }}
           >
-            <div className="fee-name">
+            <div className="fee-name d-flex flex-grow-1 ion-align-content-center">
               <p>FAST</p>
             </div>
-            <div className="fee-amount">
+            <div className="fee-amount d-flex flex-column ion-align-items-end">
               <span className="fee-time">~ 1 minute</span>
               <span className="sats-vbyte">{fastFee} sat/vByte</span>
             </div>
           </IonItem>
 
           <IonItem
-            className={`ion-no-margin ${mediumFeeSelected}`}
+            className={`ion-no-margin mt-2 mb-3 mx-7 d-flex ion-justify-content-between ${mediumFeeSelected}`}
             onClick={() => {
               setSelectedFee(mediumFee);
               setSelectedItem('medium');
               close();
             }}
           >
-            <div className="fee-name">
+            <div className="fee-name flex-grow-1 d-flex ion-align-content-center">
               <p>MEDIUM</p>
             </div>
-            <div className="fee-amount">
+            <div className="fee-amount d-flex flex-column ion-align-items-end">
               <span className="fee-time">~ 10 minutes</span>
               <span className="sats-vbyte">{mediumFee} sat/vByte</span>
             </div>
           </IonItem>
 
           <IonItem
-            className={`ion-no-margin ${slowFeeSelected}`}
+            className={`ion-no-margin mt-2 mb-3 mx-7 d-flex ion-align-content-between ${slowFeeSelected}`}
             onClick={() => {
               setSelectedFee(slowFee);
               setSelectedItem('slow');
               close();
             }}
           >
-            <div className="fee-name">
+            <div className="fee-name flex-grow-1 d-flex ion-align-content-center">
               <p>SLOW</p>
               <span></span>
             </div>
-            <div className="fee-amount">
+            <div className="fee-amount d-flex flex-column ion-align-items-end">
               <span className="fee-time">~ 25 minutes</span>
               <span className="sats-vbyte">{slowFee} sat/vByte</span>
             </div>
           </IonItem>
         </IonList>
       </IonContent>
-      <IonFooter className="ion-no-border ion-justify-content-center">
+      <IonFooter className="ion-no-border ion-justify-content-center py-2 pb-4 px-6 ">
         <IonButton className="sub-button" data-cy="sub-button" onClick={() => setShowCustomFeeInput(true)}>
           CUSTOM FEE
         </IonButton>
