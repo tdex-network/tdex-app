87c0066281f7a1a87e3fa9860a0be5a3f0600cf9
libreleon <libreleon@users.noreply.github.com>
2022-02-14 22:59:34 -0800

    Update test

--- test/services/wallet.spec.ts
+++ test/services/wallet.spec.ts
@@ -1,29 +1,15 @@
 import axios from 'axios';
 
 import { getRecommendedFees } from '../../src/redux/services/walletService';
+import { APIURL } from '../test-utils';
 
 describe('Wallet Service', () => {
-  it('should return 3 different target fees for mainnet', async () => {
-    const recommendedFees = await getRecommendedFees('liquid');
+  it('should return the expected target fees for CustomFeeModal', async () => {
+    const recommendedFees = await getRecommendedFees(APIURL);
 
-    expect(recommendedFees).toHaveProperty('fastestFee');
-    expect(recommendedFees).toHaveProperty('halfHourFee');
-    expect(recommendedFees).toHaveProperty('hourFee');
-  });
-  it('should return 3 different target fees for testnet', async () => {
-    const recommendedFees = await getRecommendedFees('testnet');
-
-    expect(recommendedFees).toHaveProperty('fastestFee');
-    expect(recommendedFees).toHaveProperty('halfHourFee');
-    expect(recommendedFees).toHaveProperty('hourFee');
-  });
-
-  it('should return 3 different target fees for regtest', async () => {
-    const recommendedFees = await getRecommendedFees('testnet');
-
-    expect(recommendedFees).toHaveProperty('fastestFee');
-    expect(recommendedFees).toHaveProperty('halfHourFee');
-    expect(recommendedFees).toHaveProperty('hourFee');
+    expect(recommendedFees).toHaveProperty('1');
+    expect(recommendedFees).toHaveProperty('10');
+    expect(recommendedFees).toHaveProperty('25');
   });
 
   it('should return default target fees on error', async () => {
@@ -31,10 +17,10 @@ describe('Wallet Service', () => {
     jest.spyOn(axios, 'get').mockRejectedValue(new Error('error'));
     jest.spyOn(console, 'error').mockImplementation();
 
-    const recommendedFees = await getRecommendedFees('liquid');
+    const recommendedFees = await getRecommendedFees(APIURL);
 
-    expect(recommendedFees.fastestFee).toEqual(0.1);
-    expect(recommendedFees.halfHourFee).toEqual(0.1);
-    expect(recommendedFees.hourFee).toEqual(0.1);
+    expect(recommendedFees['1']).toEqual(0.1);
+    expect(recommendedFees['10']).toEqual(0.1);
+    expect(recommendedFees['25']).toEqual(0.1);
   });
 });
