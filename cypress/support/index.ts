3b59544af15bda01894dae93b323704e7a650e53
Janaka-Steph <rochestephane@yahoo.fr>
2022-06-07 21:38:09 +0100

    reput support/index content

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,1 +1,28 @@
+/// <reference types="cypress" />
+
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
 import './commands';
+import type fixtures from '../fixtures/fixtures.json';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      launchWallet(opts?: Partial<typeof fixtures.localStorage>): Chainable;
+      faucet(address: string, amount: number, asset?: string): Chainable;
+    }
+  }
+}
