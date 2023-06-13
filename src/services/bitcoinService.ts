7e77d03fffde6ee6d5f4edd6402844a599814e51
Janaka-Steph <kakobaba1212@protonmail.com>
2023-06-13 20:32:23 +0200

    withDynamicFederation true

--- src/services/bitcoinService.ts
+++ src/services/bitcoinService.ts
@@ -37,12 +37,16 @@ export class BitcoinService {
   static async getPeginModule(network: NetworkString): Promise<ElementsPegin> {
     let peginModule: ElementsPegin;
     if (network === 'liquid') {
-      peginModule = new ElementsPegin(await ElementsPegin.withGoElements(), await ElementsPegin.withLibwally());
+      peginModule = new ElementsPegin(
+        await ElementsPegin.withGoElements(),
+        await ElementsPegin.withLibwally(),
+        ElementsPegin.withDynamicFederation(true)
+      );
     } else {
       peginModule = new ElementsPegin(
         await ElementsPegin.withGoElements(),
         await ElementsPegin.withLibwally(),
-        ElementsPegin.withDynamicFederation(false),
+        ElementsPegin.withDynamicFederation(true),
         ElementsPegin.withTestnet(LBTC_ASSET[network].assetHash),
         ElementsPegin.withFederationScript(getFedPegScript(network))
       );
