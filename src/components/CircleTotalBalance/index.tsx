dbea789d3f36b836f2c17ae92b080782963df571
Janaka-Steph <kakobaba1212@protonmail.com>
2023-06-13 19:23:22 +0200

    animate total balance only after restoration

--- src/components/CircleTotalBalance/index.tsx
+++ src/components/CircleTotalBalance/index.tsx
@@ -1,8 +1,10 @@
+import './style.scss';
 import { IonGrid, IonRow } from '@ionic/react';
+import classNames from 'classnames';
 import React from 'react';
 
 import TdexLogo from '../../assets/img/tdex_logo_black.svg';
-import './style.scss';
+import { useAppStore } from '../../store/appStore';
 
 interface CircleTotalBalanceProps {
   fiatBalance: string;
@@ -11,8 +13,16 @@ interface CircleTotalBalanceProps {
 }
 
 const CircleTotalBalance: React.FC<CircleTotalBalanceProps> = ({ fiatBalance, lbtcUnit, totalBalance }) => {
+  const isFetchingUtxos = useAppStore.getState().isFetchingUtxos;
+  const isFetchingTransactions = useAppStore.getState().isFetchingTransactions;
+
   return (
-    <div className="circle-total-balance">
+    <div
+      className={classNames('circle-total-balance', {
+        animate: !(isFetchingTransactions || isFetchingUtxos),
+        'animate-not': isFetchingTransactions || isFetchingUtxos,
+      })}
+    >
       <div className="ion-align-items-center ion-justify-content-center">
         <img src={TdexLogo} alt="tdex logo" />
         <IonGrid>
